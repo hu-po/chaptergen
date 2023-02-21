0489167c46cf91395724915a8b1bdd6cb2f7ce61
hupo-windows <8493427+hu-po@users.noreply.github.com>
2023-02-21 12:31:02 -0600

    not working on weird characters

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # chaptergen
+
 Creates video chapters from a .SRT file
+
+```
+python str_to_chapters.py \
+    input.srt \
+    output.txt
+```
+
+```
+python srt_to_chapters.py "C:\Users\ook\Downloads\test.srt" "C:\Users\ook\Downloads\test.txt"
+```
--- srt_to_chapters.py
+++ srt_to_chapters.py
@@ -1,15 +1,26 @@
+import argparse
 import re
 
-# Read the input text from a .srt file
-with open('input.srt', 'r') as file:
+# Set up argparse
+parser = argparse.ArgumentParser(description='Convert a subtitle file to a formatted text file.')
+parser.add_argument('input_file',
+        metavar='input_file', type=str, help='The input subtitle file in .srt format')
+parser.add_argument('output_file',
+        metavar='output_file', type=str, help='The output file to write formatted text to')
+args = parser.parse_args()
+
+# Read the input text from the specified file
+with open(args.input_file, 'r') as file:
     input_text = file.read()
 
 # Use regular expression to find the timestamp and text on separate lines
-pattern = r'\d{2}:\d{2}:\d{2},\d{3}\s-->\s\d{2}:\d{2}:\d{2},\d{3}\n(.+?(?=\n\d{2}:\d{2}:\d{2},\d{3}\s-->)|.+)'
+pattern = r'\d{2}:\d{2}:\d{2},\d{3}\s*-->\s*\d{2}:\d{2}:\d{2},\d{3}\n(.+?)(?=\n\d{2}:\d{2}:\d{2},\d{3}\s*-->\s*\d{2}:\d{2}:\d{2},\d{3}\n|$)'
 matches = re.findall(pattern, input_text, re.DOTALL)
 
-# Iterate through matches to print output
-for i, match in enumerate(matches):
-    timestamp = re.search(r'\d{2}:\d{2}:\d{2},\d{3}', match).group()
-    text = re.sub(r'\n', ' ', match).replace(timestamp, '').strip()
-    print(f'{timestamp} - {text}')
+# Write output to the specified file
+with open(args.output_file, 'w') as file:
+    for i, match in enumerate(matches):
+        print(f'match {i}: {match}')
+        timestamp = re.search(r'(\d{2}:\d{2}:\d{2},\d{3})\s*-->\s*\d{2}:\d{2}:\d{2},\d{3}', match).group(1)
+        text = re.sub(r'\n', ' ', match).replace(timestamp, '').strip()
+        file.write(f'{timestamp} - {text}\n')
